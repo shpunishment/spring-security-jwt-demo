2f8f71100205f882aecff6b3527f801a2b5f0044
shpunishment <shpunishment@gmail.com>
2020-09-15 14:47:54 +0800

    添加配置

--- src/main/java/com/shpun/jwt/SpringSecurityJwtDemoApplication.java
+++ src/main/java/com/shpun/jwt/SpringSecurityJwtDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.shpun.jwt")
+@MapperScan("com.shpun.jwt.mapper")
 public class SpringSecurityJwtDemoApplication {
 
     public static void main(String[] args) {
